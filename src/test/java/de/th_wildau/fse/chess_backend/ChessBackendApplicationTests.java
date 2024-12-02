11ca32aac26bd74421b74bd4bc349004a412d290
Artur Feldt <Artur.Feldt@gmail.com>
2024-12-02 12:04:15 +0100

    initial commit

---
+++ src/test/java/de/th_wildau/fse/chess_backend/ChessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.th_wildau.fse.chess_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
