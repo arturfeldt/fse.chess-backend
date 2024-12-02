a51994aea4a8adc6f80e18efb3df2f2d59793382
Artur Feldt <Artur.Feldt@gmail.com>
2024-12-02 12:55:10 +0100

    moved database properties to application.properties

--- src/main/java/de/th_wildau/fse/chess_backend/ChessBackendApplication.java
+++ src/main/java/de/th_wildau/fse/chess_backend/ChessBackendApplication.java
@@ -1,9 +1,5 @@
 package de.th_wildau.fse.chess_backend;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Properties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChessBackendApplication {
 
 	public static void main(String[] args) {
-		String configPath = "." + File.separatorChar + "config" + File.separatorChar +"db.properties";
-		Properties dbProp = new Properties();
-		
-		try (FileInputStream fis = new FileInputStream(configPath)){
-            dbProp.load(fis);
-			System.out.println(dbProp.toString());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
 		SpringApplication.run(ChessBackendApplication.class, args);
 	}
 
